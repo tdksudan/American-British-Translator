12145acd8fb080f80db4d44254a74d297f5b95ac
Sudan Tandukar <sudan.tandukar@patancollege.edu.np>
2025-08-06 02:52:08 +0000

    rooutes completed

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,27 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      const {text, locale} = req.body;
+
+      //Validate presence of fields
+      if (text === undefined || locale === undefined){
+        return res.json({error: 'Required fiedls(s) missing'});
+
+      }
+
+      //Validate empty text
+      if (text.trim() === ''){
+        return res.json({error: 'No text to translate'});
+      }
+
+      //Validate locale 
+      if (locale !== 'american-to-british' && locale !== 'british-to-american'){
+        return res.json({ error: 'Invalid value for locale field'});
+      }
+
+      //Perform translation
+      const translation = translator.translate(text, locale);
+
+      return res.json({ text, translation});
     });
 };
